e5dff72503adb6f147c7e6eb119e55e2c4fe853c
maiconio <maiconscosta@gmail.com>
2017-09-23 11:46:28 +0200

    Automatic creation of minio volumes

--- s3.sh
+++ s3.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-docker volume create minio-data
-docker volume create minio-config
-docker run -p 9000:9000 --name minio1 --mount source=minio-data,target=/data --mount source=minio-config,target=/root/.minio minio/minio server /data
+echo "Checking if minio-data volume is created:"
+docker volume inspect minio-data
+if [ $? -ne 0 ]; then
+    echo "Creating minio-data volume."
+    docker volume create minio-data
+fi
+
+if [ $? -eq 0 ]; then
+    echo "minio-data volume setup complete"
+else
+    echo "Error while checking for minio-data volume."
+    exit 1
+fi
+
+echo "Checking if minio-config volume is created:"
+docker volume inspect minio-config
+if [ $? -ne 0 ]; then
+    echo "Creating minio-config volume."
+    docker volume create minio-config
+fi
+
+if [ $? -eq 0 ]; then
+    echo "minio-config volume setup complete"
+else
+    echo "Error while checking for minio-config volume."
+    exit 1
+fi
+
+echo "Starting minio"
+docker run -p 9000:9000 --rm --name minio1 --mount source=minio-data,target=/data --mount source=minio-config,target=/root/.minio minio/minio server /data
